3ce4a903e82b727833c82ffe4c14b78bf5b3c059
Danny Willems <contact@danny-willems.be>
2016-05-23 17:06:15 +0200

    New config with package and Makefile seperation

--- app/ml/test.ml
+++ app/ml/test.ml
@@ -1,10 +1,10 @@
 let on_device_ready _ =
-  let i = In_app_browser.t () in
+  let i = Cordova_in_app_browser.t () in
   (* Clear cache and show InAppBrowser location bar *)
-  let opt = In_app_browser.options_list_to_str [In_app_browser.location false ;
-  In_app_browser.ai_clear_cache true] in
+  let opt = Cordova_in_app_browser.options_list_to_str [Cordova_in_app_browser.location false ;
+  Cordova_in_app_browser.ai_clear_cache true] in
   (* Opens in the Cordova WebView if the URL is in the white list *)
-  i#open_ "https://ocaml.org" (In_app_browser.target_self) opt;
+  i#open_ "https://github.com/dannywillems/ocaml-cordova-plugin-list" (Cordova_in_app_browser.target_self) opt;
   Js._false
 
 let _ =
