5779fa9e6d884d851bf31c140c42332b572f1ba1
Danny Willems <contact@danny-willems.be>
2016-05-27 21:51:46 +0200

    Add Cordova plugin and change device ready event listener

--- app/ml/test.ml
+++ app/ml/test.ml
@@ -1,12 +1,9 @@
-let on_device_ready _ =
+let on_device_ready () =
   let i = Cordova_in_app_browser.t () in
   (* Clear cache and show InAppBrowser location bar *)
   let opt = Cordova_in_app_browser.options_list_to_str [Cordova_in_app_browser.location false ;
   Cordova_in_app_browser.ai_clear_cache true] in
   (* Opens in the Cordova WebView if the URL is in the white list *)
-  i#open_ "https://github.com/dannywillems/ocaml-cordova-plugin-list" (Cordova_in_app_browser.target_self) opt;
-  Js._false
+  i#open_ "https://github.com/dannywillems/ocaml-cordova-plugin-list" (Cordova_in_app_browser.target_self) opt
 
-let _ =
-  Dom.addEventListener Dom_html.document (Dom.Event.make "deviceready")
-  (Dom_html.handler on_device_ready) Js._false
+let _ = Cordova.Event.device_ready on_device_ready
